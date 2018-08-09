9f32c9eb6561377a9fbb17ed8249c0dbdbbb8eca
Matthias Koch <ithrowexceptions@gmail.com>
2018-08-09 02:44:19 +0200

    ++

---
+++ QFCA/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace QFCA
+{
+    public class Class1
+    {
+        public void A()
+        {
+//            IEnumerable list = default(IEnumerable);
+            
+            var paragraph =
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.";
+            
+            var containsOnlyLetters = !paragraph.Any(x => !char.IsLetter(x));
+        }
+        
+        
+        public IEnumerable<string> B (IEnumerable<string> collection)
+        {
+            foreach (var str in collection)
+            {
+                if (string.IsNullOrEmpty(str))
+                    continue;
+
+                yield return str;
+            }
+        }
+    }
+}
